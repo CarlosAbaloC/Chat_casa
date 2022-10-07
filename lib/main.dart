007c1d7d969cbd590703bf9bbac87ca65274df51
Carlos <carlos.abalocordero@gmail.com>
2022-10-08 00:37:22 +0200

    Flutterfire

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:chat_casa_v/src/App.dart';
 import 'package:chat_casa_v/src/AppMaterial.dart';
-
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async{
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const App());
   //runApp(const AppMaterial());
 }
